20fefb64022e8f723fb6f62914fe2cd0b93f6ddc
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-15 21:25:27 +0330

    create ICategoryService

---
+++ Backend/Application/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.Category;
+using Application.DTOs.product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDto>> GetAllAysnc();
+        Task<List<CategoryDto>> GetByIndexAsync(int index);
+        Task<CategoryDto?> GetByIdAsync(Guid id);
+        Task<CategoryItemsDto> GetCateroryItemsAysnc(Guid id);
+        Task<bool> ChangeStatusAsync(Guid id);
+        Task<Guid> CreateAsync(CategoryCreateDto dto);
+        Task<bool> UpdateAsync(CategoryUpdateDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
