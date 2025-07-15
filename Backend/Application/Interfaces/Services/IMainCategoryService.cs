6b2035e52b230ae7a8b620c27c655d2982c902ea
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-15 21:57:34 +0330

    creating IMainCategoryService

---
+++ Backend/Application/Interfaces/Services/IMainCategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.MainCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IMainCategoryService
+    {
+        Task<List<MainCategoryDto>> GetAllAysnc();
+        Task<MainCategoryDto?> GetByIdAsync(Guid id);
+        Task<MainCategoryCategoriesDto> GetCateroryCategoriesAysnc(Guid id);
+        Task<bool> ChangeStatusAsync(Guid id);
+        Task<Guid> CreateAsync(MainCategoryCreateDto dto);
+        Task<bool> UpdateAsync(MainCategoryUpdateDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
