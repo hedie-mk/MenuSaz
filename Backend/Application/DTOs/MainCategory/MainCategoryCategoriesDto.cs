3470c6f840c66c604da6b08f69e65d19af9e3545
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-15 21:57:10 +0330

    creating mainCategory Dtos

---
+++ Backend/Application/DTOs/MainCategory/MainCategoryCategoriesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.MainCategory
+{
+    public class MainCategoryCategoriesDto
+    {
+        public string Name { get; set; }
+        public List<string>? Categories { get; set; } = new List<string>();
+    }
+}
---
+++ Backend/Application/DTOs/MainCategory/MainCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.MainCategory
+{
+    public class MainCategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public List<string>? Categories { get; set; } = new List<string>();
+        public int CategoriesLength { get; set; }
+    }
+}
