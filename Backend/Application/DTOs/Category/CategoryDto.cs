4d906f73ddedbda63411892eba0b096a1d040e20
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-15 21:22:54 +0330

    create Category Dtos

---
+++ Backend/Application/DTOs/Category/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Category
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public Guid ParentCategoryId { get; set; }
+        public List<string>? Items { get; set; } = new List<string>();
+        public int ItemsLength { get; set; }
+    }
+}
