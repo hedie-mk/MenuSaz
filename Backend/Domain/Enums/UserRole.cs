797589e7c32dacda5942591b1725ce7518139733
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-15 21:20:09 +0330

    create role enum

---
+++ Backend/Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum UserRole
+    {
+        Manager = 0,
+        Staff = 1
+    }
+}
