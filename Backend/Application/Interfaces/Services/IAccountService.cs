4ffb5d9654ebc27ec06bfd21c13268744b60d78b
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-16 19:59:19 +0330

    creating IAccountService

---
+++ Backend/Application/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IAccountService
+    {
+        Task<AccountDto> GetAllAysnc();
+        Task<AccountDto?> AuthenticateAsync(string username, string password);
+        Task<Guid> CreateAccountAysnc(AccountCreateDto dto);
+        Task<bool> UpdateAccountAysnc(AccountUpdateDto dto);
+        Task<bool> DeleteAccountAysnc(Guid id);
+
+    }
+}
