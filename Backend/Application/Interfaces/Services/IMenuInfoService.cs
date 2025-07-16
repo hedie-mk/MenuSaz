c8d683aa3a2c85832c74a7b7e24b018483069017
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-16 15:25:13 +0330

    creating ImenuinfoService

---
+++ Backend/Application/Interfaces/Services/IMenuInfoService.cs
@@ -0,0 +1,17 @@
+﻿
+using Application.DTOs.MenuInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IMenuInfoService
+    {
+        Task<List<MenuInfoDto>> GetMenuInfoAsync();
+        Task<Guid> CreateMenuInfoAsync(MenuInfoCreateDto dto);
+        Task<bool> UpdateMenuInfoAsync(MenuInfoUpdateDto dto);
+    }
+}
