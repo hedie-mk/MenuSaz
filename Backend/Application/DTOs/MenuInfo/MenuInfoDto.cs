0aa6b90f41bf8cb60c3f12396429e9f7604072c7
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-16 15:24:53 +0330

    creating MenuInfo Dtos and folder

---
+++ Backend/Application/DTOs/MenuInfo/MenuInfoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.MenuInfo
+{
+    public class MenuInfoDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string? WorkHour { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? SiteDescription { get; set; }
+        public string? SocialMedia { get; set; }
+        public string Logo { get; set; } = string.Empty;
+    }
+}
