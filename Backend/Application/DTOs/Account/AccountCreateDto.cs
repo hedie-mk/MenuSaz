a702e9378d7f06cedadfb7ba3a6e2e9299b12f72
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-16 19:59:01 +0330

    creating account dtos

---
+++ Backend/Application/DTOs/Account/AccountCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Account
+{
+    public class AccountCreateDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.Staff;
+    }
+}
---
+++ Backend/Application/DTOs/Account/AccountDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Account
+{
+    public class AccountDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        public string? Phone { get; set; } 
+        public string Role { get; set; } = string.Empty ;
+        public DateTime CreatedAt { get; set; }
+    }
+}
