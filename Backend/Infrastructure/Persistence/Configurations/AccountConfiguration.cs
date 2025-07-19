01feb20e8c1bd75296a4bdcd809aa40b2d761008
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-19 18:45:46 +0330

    create entities configuration

---
+++ Backend/Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.Property(a => a.UserName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(a => a.Role)
+                .HasConversion<string>();
+
+            builder.Property(a => a.Password)
+                .IsRequired();
+
+            builder.HasIndex(a => a.UserName)
+                .IsUnique();
+        }
+    }
+}
---
+++ Backend/Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Name)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property(c => c.State)
+                .HasConversion<string>();
+
+            builder.HasOne(c => c.ParentCategory)
+                .WithMany(m => m.Categories)
+                .HasForeignKey(c => c.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(c => c.Items)
+                .WithOne(c => c.Category)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+        }
+    }
+}
