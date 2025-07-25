5676c0f2b8c7f9c87b7a5b1396a168bd9e8c70d6
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-25 16:33:53 +0330

    add DiactiveDateTime prop to both entities

--- Backend/Domain/Entities/Item.cs
+++ Backend/Domain/Entities/Item.cs
@@ -16,6 +16,7 @@ namespace Domain.Entities
         public decimal? DiscountedPrice { get; set; }
         public string? Photo { get; set; }
         public required State State { get; set; } = State.active;
+        public DateTime? DiactiveDateTime { get; set; }
         public Category? Category { get; set; }
         public Guid? CategoryId { get; set; }
         public Item()
