f00d9dbb28f6f2df737c8ffe3e79e6c46d09b0b5
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-26 18:25:14 +0330

    made id nullable

--- Backend/Application/DTOs/Account/ChangePasswordDto.cs
+++ Backend/Application/DTOs/Account/ChangePasswordDto.cs
@@ -8,7 +8,7 @@ namespace Application.DTOs.Account
 {
     public class ChangePasswordDto
     {
-        public Guid Id { get; set; }
-        public string Password { get; set; }
+        public Guid? Id { get; set; }
+        public required string Password { get; set; }
     }
 }
