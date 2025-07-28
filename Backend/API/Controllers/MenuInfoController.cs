2b5e4314fe00727d88c6579a47cc8f8a6f92b48f
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-28 15:33:23 +0330

    create menuInfo controller

---
+++ Backend/API/Controllers/MenuInfoController.cs
@@ -0,0 +1,42 @@
+﻿using Application.DTOs.MenuInfo;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    [Authorize]
+    public class MenuInfoController : Controller
+    {
+        private readonly IMenuInfoService _menuInfoService;
+        public MenuInfoController(IMenuInfoService menuInfoService)
+        {
+            _menuInfoService = menuInfoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuInfo()
+        {
+            var info = await _menuInfoService.GetMenuInfoAsync();
+            return Ok(info);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> Create(MenuInfoCreateDto dto)
+        {
+            var id = await _menuInfoService.CreateMenuInfoAsync(dto);
+            return Ok(id);
+        }
+        [HttpPut]
+        [Authorize(Roles ="Manager")]
+        public async Task<IActionResult> Update(MenuInfoUpdateDto dto)
+        {
+            var result = await _menuInfoService.UpdateMenuInfoAsync(dto);
+            return result ? Ok() : BadRequest();
+        }
+
+
+    }
+}
