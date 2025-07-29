bab1e2c6840773435f2e3e8f36e149c608052340
Hedieh Malekan <hediehmalekan@gmail.com>
2025-07-29 15:57:13 +0330

    create IFileService interface

---
+++ Backend/Application/Interfaces/Helpers/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Application.Interfaces.Helpers
+{
+    public interface IFileService
+    {
+        Task<string> SaveFileAsync(IFormFile file, FileType type);
+        Task DeleteFileAsync(string fileName, FileType type);
+    }
+}
