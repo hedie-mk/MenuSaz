ee97bc478c69e18fe86b9f62702dc6ddadf60f07
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-03 16:35:46 +0330

    add the function to private route to check the token

--- MenuFrontend/src/routes/PrivateRoute.tsx
+++ MenuFrontend/src/routes/PrivateRoute.tsx
@@ -2,11 +2,19 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import type { RootState } from "../app/app";
 import type { ReactNode } from "react";
+ import { useAppDispatch } from "../app/hooks";
+ import { isTokenExpired } from "../utils/auth/TokenExpire";
+ import { logout } from "../features/auth/authSlice";
 
 const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const token = useSelector((state: RootState) => state.auth.token);
+  const dispatch = useAppDispatch();
+   if (!token || isTokenExpired(token)) {
+    dispatch(logout()); // پاک کردن توکن
+    return <Navigate to="/login" replace />;
+  }
 
-  return token ? children : <Navigate to="/login" replace />;
+  return children;
 };
 
 export default PrivateRoute;
