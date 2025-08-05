f348a673d05f1b94696768838f924743e816b9f0
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-05 15:53:28 +0330

    new functionality like search and filter and pagination

--- MenuFrontend/src/pages/dashboard/products/Products.tsx
+++ MenuFrontend/src/pages/dashboard/products/Products.tsx
@@ -4,6 +4,7 @@ import ProductTable from "../../../components/dashboard/products/productTable";
 import { useState } from "react";
 import { useGetProductsQuery } from "../../../features/Products/productTable/productApi";
 import { useNavigate } from "react-router-dom";
+
 const categories = [
   { name: "همه", value: "" },
   { name: "قهوه", value: "coffee" },
@@ -17,12 +18,65 @@ const [category, setCategory] = useState("");
 const [search , setSearch] = useState("");
 const navigate = useNavigate();
 
-const filteredProducts = category
-    ? data?.filter((p) => p.categoryName === category)
-    : data;
 
+const [currentPage, setCurrentPage] = useState(1);
+const itemsPerPage = 5; // چند محصول در هر صفحه
+
+
+
+var filteredProducts = undefined;
+if(category){
+    filteredProducts = data?.filter((p) => p.categoryName === category );
+}
+else if(search){
+    filteredProducts = data?.filter((p)=> p.name.includes(search))
+}
+else{
+    filteredProducts = data;
+}
+// محاسبه محصولات این صفحه
+const indexOfLastItem = currentPage * itemsPerPage;
+const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+const currentItems = filteredProducts?.slice(indexOfFirstItem, indexOfLastItem);
+
+// تغییر صفحه
+var totalPages = 0;
+if(data) totalPages = Math.ceil(data?.length / itemsPerPage);
+const getPageNumbers = () => {
+    const pages = [];
+    const maxVisible = 5;
+
+    if (totalPages <= maxVisible) {
+      for (let i = 1; i <= totalPages; i++) pages.push(i);
+    } else {
+      if (currentPage <= 3) {
+        pages.push(1, 2, 3, 4, 5, "...", totalPages);
+      } else if (currentPage >= totalPages - 2) {
+        pages.push(
+          1,
+          "...",
+          totalPages - 4,
+          totalPages - 3,
+          totalPages - 2,
+          totalPages - 1,
+          totalPages
+        );
+      } else {
+        pages.push(
+          1,
+          "...",
+          currentPage - 1,
+          currentPage,
+          currentPage + 1,
+          "...",
+          totalPages
+        );
+      }
+    }
+    return pages;
+  };
 return(
-    <div className="w-[1120px] p-6 ">
+    <div className="w-[1120px] p-5.5 ">
         <div className="grid grid-cols-3">
             <div className="flex justify-between col-span-2 items-center mb-4">
                 <div className="flex gap-4 items-center ">
@@ -38,7 +92,7 @@ return(
             </div>
             <div className="col-span-1 flex mb-4 justify-end items-end">
                 <button
-                    onClick={() => navigate("/create-product")}
+                    onClick={() => navigate("create")}
                     className=" lg:w-[230px] w-[150px] bg-yellow-400 hover:bg-yellow-500  text-white font-bold py-2 px-6 rounded-xl text-lg shadow "
                 >
                     ساخت محصول
@@ -46,7 +100,33 @@ return(
             </div>
         </div>
         <div>
-            <ProductTable isLoading={isLoading} filteredItem={filteredProducts} tHead={thead}/>
+            <ProductTable isLoading={isLoading} filteredItem={currentItems} tHead={thead}/>
+            
+            
+            <div className="flex justify-center mt-2">
+                <div className="flex items-center gap-2 bg-[#D8D4FF] rounded-full px-4 py-1">
+                {getPageNumbers().map((num, idx) =>
+                    num === "..." ? (
+                    <span key={idx} className="px-3 text-purple-800">
+                        ...
+                    </span>
+                    ) : (
+                    <button
+                        key={idx}
+                        onClick={() => setCurrentPage(num as number)}
+                        className={`w-7 h-7 duration-400 ease-in-out  rounded-full flex items-center justify-center text-sm font-medium transition 
+                        ${
+                            currentPage === num
+                            ? "bg-[#0C1086] text-white hover:cursor-pointer"
+                            : "text-[#0C1086] hover:cursor-pointer"
+                        }`}
+                    >
+                        {num}
+                    </button>
+                    )
+                )}
+                </div>
+            </div>
         </div>
     </div>
 )
