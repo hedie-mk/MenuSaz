40c74ee8a0a9dba62a01c69dd365ac78d126901f
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-06 19:59:35 +0330

    implementing category item box

--- MenuFrontend/src/components/dashboard/Category/CategoryItemsBox.tsx
+++ MenuFrontend/src/components/dashboard/Category/CategoryItemsBox.tsx
@@ -1,8 +1,31 @@
+import { useGetCategoryItemsQuery } from "../../../features/Category/categoryApi"
+
+type CategoryItemsBox = {
+    selectedRowName : string | null,
+    selectedRowId : string
+}
+
+export default function CategoryItemsBox({selectedRowName , selectedRowId} : CategoryItemsBox){
+    console.log(selectedRowId)
+    const {data : items , isLoading} = useGetCategoryItemsQuery(selectedRowId);
+    console.log(items)
 
 
-
-export default function CategoryItemsBox(){
     return(
-        <></>
+        <div className="border border-yellow-400 text-yellow-500 font-bold rounded-2xl px-3 py-3 mx-2">
+            <h2>محصولات موجود در {selectedRowName}</h2>
+            <div className="flex flex-wrap mt-3">
+                {isLoading ? (
+                    <p className="text-center py-6">
+                        در حال بارگذاری...
+                    </p>
+                ) : (
+                    items?.itemsName.map(i => (
+                        <div className="bg-[#FFF0B3] text-[#0C1086] font-light px-3 py-1 m-1 rounded-lg">{i}</div>
+                    ))
+                )
+                }
+            </div>
+        </div>
     )
 }
