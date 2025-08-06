fed2314c1390bd8050bb24058a17beea8836d149
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-06 16:03:38 +0330

    adding category header props

--- MenuFrontend/src/pages/dashboard/Category/Category.tsx
+++ MenuFrontend/src/pages/dashboard/Category/Category.tsx
@@ -1,12 +1,25 @@
 import CategoryHeader from "../../../components/dashboard/Category/CategoryHeader"
 import CategoryTable from "../../../components/dashboard/Category/CategoryTable"
 import CategoryItemsBox from "../../../components/dashboard/Category/CategoryItemsBox"
-
+import { useState } from "react"
 export default function Category(){
+    const [search , setSearch] = useState("")
+    const [mainCategory , setMainCategory] = useState("")
+
+
+
+
+
     return(
     <div className="w-[1120px] p-5.5 ">
         <div className="grid grid-cols-5">
-            <CategoryHeader/>
+            <CategoryHeader 
+            search={search}
+            setSearch={setSearch}
+            options={[]}
+            mainCategoryFilter={mainCategory}
+            setMainCategoryFilter={setMainCategory}
+            />
         </div>
         <div className="grid grid-cols-4">
             <div className="col-span-3">
