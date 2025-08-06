5082c28ab244e5a50cc1d0259707581347f7c8be
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-06 19:59:11 +0330

    deleting filter input and unused props

--- MenuFrontend/src/components/dashboard/Category/CategoryHeader.tsx
+++ MenuFrontend/src/components/dashboard/Category/CategoryHeader.tsx
@@ -1,26 +1,17 @@
-import FilterInput from "../shared/filterInput"
+
 import SearchInput from "../shared/searchInput"
 import { useNavigate } from "react-router-dom"
-type Option = {
-  name: string;
-  id: string; 
-}
+
 
 type CategoryHeaderProps = {
     search : string,
     setSearch : (value : string) => void,
-    options : Option[] | undefined,
-    mainCategoryFilter : string,
-    setMainCategoryFilter : (value : string) => void,
 }
 
 
 export default function CategoryHeader({
     search,
-    setSearch,
-    options,
-    mainCategoryFilter,
-    setMainCategoryFilter
+    setSearch
 } : CategoryHeaderProps){
     const navigate = useNavigate();
 
@@ -29,19 +20,14 @@ export default function CategoryHeader({
 
     return(
         <>
-            <div className="flex justify-between col-span-3 items-center mb-4">
+            <div className="flex justify-between col-span-3 items-center ">
                 <div className="flex gap-4 items-center ">
                     <SearchInput search={search}
                                 setSearch={setSearch}
                     />
-
-                    <FilterInput options={options}
-                                categoryFilter={mainCategoryFilter}
-                                setCategoryFilter={setMainCategoryFilter}
-                    />
                 </div>
             </div>
-            <div className="col-span-2 flex mb-4 justify-end items-end">
+            <div className="col-span-2 flex justify-end items-end">
                 <div className="flex gap-3 items-center ">
                     <button
                         onClick={() => navigate("mainCategory/Create")}
