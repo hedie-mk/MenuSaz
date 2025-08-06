8d578f786b2cadef0c5ad9256489ae931121cd5c
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-06 15:17:15 +0330

    deleting unused imports

--- MenuFrontend/src/components/dashboard/products/AddCategoryModal.tsx
+++ MenuFrontend/src/components/dashboard/products/AddCategoryModal.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useState } from "react";
 import { useGetCategoriesQuery } from "../../../features/Category/categoryApi";
-import { useAddCategoryToProductMutation } from "../../../features/Products/productApi";
 type AddCategoryProps = {
   isOpen: boolean;
   selectedName : string | null;
