cb99654a25c45eb390e8c5153d12c5bdaa5fc0a5
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-09 19:04:33 +0330

    changing checkbox style

--- MenuFrontend/src/components/dashboard/products/productTable.tsx
+++ MenuFrontend/src/components/dashboard/products/productTable.tsx
@@ -102,10 +102,14 @@ export default function ProductTable({isLoading , filteredItem , tHead} : TableP
                         <Trash2 className="text-[#0C1086] hover:text-red-700 duration-200 ease-in "/>
                       </button>
                       
-
-                      <label className="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" className="sr-only peer" checked={item.state === 'active'} onChange={() => changeProductStatus(item.id)} />
-                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none  peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#0C1086] dark:peer-checked:bg-[#0C1086]"></div>
+                      <label className="inline-flex cursor-pointer">
+                      <input
+                          type="checkbox"
+                          className="sr-only peer"
+                          checked={item.state === "active"}
+                          onChange={() => changeProductStatus(item.id)}
+                      />
+                      <div className="relative w-11 h-6 bg-gray-400 peer-checked:bg-[#0C1086] rounded-full after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border after:border-gray-300 after:rounded-full after:w-5 after:h-5 after:transition-all peer-checked:after:translate-x-full"></div>
                       </label>
                       
                     </td>
