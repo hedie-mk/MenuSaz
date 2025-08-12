2bc25b8582b50aa74691891befe7cfc53334a38f
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-12 22:05:16 +0330

    creating account page

---
+++ MenuFrontend/src/pages/dashboard/Account/Account.tsx
@@ -0,0 +1,29 @@
+import AccountBox from "../../../components/dashboard/Account/accountBox";
+import {jwtDecode} from "jwt-decode";
+import type { RootState } from "../../../app/app";
+import { useSelector } from "react-redux";
+
+
+export default function Account() {
+
+  const token = useSelector((state: RootState) => state.auth.token);
+  let role = "";
+
+  if (token) {
+    try {
+      const decoded = jwtDecode<{ role: string }>(token);
+      role = decoded.role;
+    } catch (error) {
+      console.error("توکن نامعتبره:", error);
+    }
+  }
+
+  return (
+    <div className="flex flex-col md:flex-row w-full max-w-[800px] gap-4 px-6 mt-5 sm:mt-10 justify-center items-center">
+      <div className="w-1/2 px-4 py-3 sm:hidden flex justify-center rounded-full bg-[#0C1086] text-white shadow-md text-sm font-bold">
+          اکانت
+      </div>
+      <AccountBox role={role}/>
+    </div>
+  );
+}
