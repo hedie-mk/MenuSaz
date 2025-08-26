851c8cd75de96408b99131128b39982e08f4c08a
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-26 19:56:15 +0330

    adding plugins

--- MenuFrontend/vite.config.ts
+++ MenuFrontend/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {qrcode} from "vite-plugin-qrcode"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    qrcode(),
   ],
+  server : {
+    port : 5173
+  }
 })
