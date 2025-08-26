4292dad75ee6235489859cea527d1911e7027881
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-27 02:19:38 +0330

    add migration and updating database

---
+++ Backend/Infrastructure/Migrations/20250826184443_updateCategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Categories");
+        }
+    }
+}
