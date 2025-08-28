c9cf9bc564e3a59c6b8f834af38091c11b8d5d7e
Hedieh Malekan <hediehmalekan@gmail.com>
2025-08-28 22:18:12 +0330

    style change

--- MenuFrontend/src/components/menu/menuHeader.tsx
+++ MenuFrontend/src/components/menu/menuHeader.tsx
@@ -9,11 +9,11 @@ type MenuHeaderProps = {
 
 export default function MenuHeader({cafeName , search , setSearch}:MenuHeaderProps){
     return(
-        <div className="flex justify-center items-center mt-5 mx-2">
+        <div className="flex justify-between items-center mt-5 mx-2">
             <div>
                 <h1 className="font-BTitr text-white text-xl"> به کافه {cafeName} خوش آمدید</h1>
             </div>
-            <div className="flex-2/3 flex w-full rounded-xl px-2 py-2 bg-[#B3B9C8]">
+            <div className="basis-2/3 flex justify-between w-full rounded-xl px-2 py-2 bg-[#B3B9C8]">
                 <input 
                 type="text"
                 value={search}
