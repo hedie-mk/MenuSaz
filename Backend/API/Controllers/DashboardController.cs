dbab6b92572deb47a469e1a3c552e6b92deb0784
Hedieh Malekan <hediehmalekan@gmail.com>
2025-10-25 21:15:48 +0330

    adding logs to all controllers

--- Backend/API/Controllers/DashboardController.cs
+++ Backend/API/Controllers/DashboardController.cs
@@ -12,17 +12,20 @@ namespace API.Controllers
         private readonly ICategoryService _categoryService;
         private readonly IItemService _itemService;
         private readonly IMenuInfoService _menuInfoService;
+        private readonly ILogger<DashboardController> _logger;
 
-        public DashboardController(ICategoryService categoryService, IItemService itemService, IMenuInfoService menuInfoService)
+        public DashboardController(ICategoryService categoryService, IItemService itemService, IMenuInfoService menuInfoService, ILogger<DashboardController> logger)
         {
             _categoryService = categoryService;
             _itemService = itemService;
             _menuInfoService = menuInfoService;
+            _logger = logger;
         }
 
         [HttpGet("Menu")]
         public async Task<IActionResult> GetMenu()
         {
+            _logger.LogInformation("Fetching all categories with their items count");
             var categories = await _categoryService.GetAllAysnc();
 
             var result = categories!.Select(c => new
@@ -37,6 +40,7 @@ namespace API.Controllers
         [HttpGet("DeactiveItems")]
         public async Task<IActionResult> GetDeactiveItems()
         {
+            _logger.LogInformation("Fetching all diactive items");
             var items = await _itemService.GetDiactiveItemsAsync();
             return Ok(items);
         }
@@ -44,6 +48,7 @@ namespace API.Controllers
         [HttpGet("DiactiveCategories")]
         public async Task<IActionResult> GetDiactiveCategories()
         {
+            _logger.LogInformation("Fetching all diactive categories");
             var items = await _categoryService.GetDiactiveCategoryAsync();
             return Ok(items);
         }
@@ -51,6 +56,7 @@ namespace API.Controllers
         [HttpGet("latestItems/{index:int}")]
         public async Task<IActionResult> GetLatestAddedItems(int index)
         {
+            _logger.LogInformation("Fetching latest added items");
             var items = await _itemService.GetLatestAddedItemsAsync(index);
             return Ok(items);
         }
@@ -58,6 +64,7 @@ namespace API.Controllers
         [HttpGet("MenuInfo")]
         public async Task<IActionResult> GetMenuInfo()
         {
+            _logger.LogInformation("Fetching cafe menu information");
             var info = await _menuInfoService.GetMenuInfoAsync();
             return Ok(info);
         }
--- Backend/API/Controllers/MainCategoryController.cs
+++ Backend/API/Controllers/MainCategoryController.cs
@@ -14,20 +14,24 @@ namespace API.Controllers
     public class MainCategoryController : Controller
     {
         private readonly IMainCategoryService _mainCategoryService;
-        public MainCategoryController(IMainCategoryService mainCategoryService)
+        private readonly ILogger<MainCategoryController> _logger;
+        public MainCategoryController(IMainCategoryService mainCategoryService , ILogger<MainCategoryController> logger)
         {
             _mainCategoryService = mainCategoryService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            _logger.LogInformation("Fetching all main categories");
             var result = await _mainCategoryService.GetAllAysnc();
             return Ok(result);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
+            _logger.LogInformation("Fetching main category with id : {id}" , id);
             var result = await _mainCategoryService.GetByIdAsync(Guid.Parse(id));
             return result != null ? Ok(result) : NotFound();
         }
@@ -36,6 +40,7 @@ namespace API.Controllers
         [Authorize(Roles = "Manager,Staff")]
         public async Task<IActionResult> Create([FromBody] MainCategoryCreateDto dto)
         {
+            _logger.LogInformation("Start creating main category");
             var id = await _mainCategoryService.CreateAsync(dto);
             return Created($"api/MainCategory/{id}", new { id });
         }
@@ -44,6 +49,7 @@ namespace API.Controllers
         [Authorize(Roles = "Manager,Staff")]
         public async Task<IActionResult> Update([FromBody] MainCategoryUpdateDto dto)
         {
+            _logger.LogInformation("Updating main category with id : {id}" , dto.Id);
             var result = await _mainCategoryService.UpdateAsync(dto);
             return result ? Ok() : NotFound();
         }
@@ -52,6 +58,7 @@ namespace API.Controllers
         [Authorize(Roles = "Manager,Staff")]
         public async Task<IActionResult> Delete(string id)
         {
+            _logger.LogInformation("Deleting main category with id : {id}", id);
             var result = await _mainCategoryService.DeleteAsync(Guid.Parse(id));
             return result ? Ok() : NotFound();
         }
@@ -60,6 +67,7 @@ namespace API.Controllers
         [Authorize(Roles = "Manager,Staff")]
         public async Task<IActionResult> ChangeStatus(string id)
         {
+            _logger.LogInformation("Changing main category status with id : {id}",id);
             var result = await _mainCategoryService.ChangeStatusAsync(Guid.Parse(id));
             return result ? Ok() : NotFound();
         }
@@ -67,6 +75,7 @@ namespace API.Controllers
         [HttpGet("getCategoryCategories/{id}")]
         public async Task<IActionResult> GetCateroryCategories(string id)
         {
+            _logger.LogInformation("Fetching all categories in main category with id : {id} ", id);
             var categories = await _mainCategoryService.GetCateroryCategoriesAysnc(Guid.Parse(id));
             return categories != null ? Ok(categories) : NotFound();
         }
@@ -74,6 +83,7 @@ namespace API.Controllers
         [HttpGet("GetDiactiveMainCategories")]
         public async Task<IActionResult> GetDiactiveCategory()
         {
+            _logger.LogInformation("Fetching all diactive main categories");
             var items = await _mainCategoryService.GetDiactiveCategoryAsync();
             return Ok(items);
         }
